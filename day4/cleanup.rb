e9682684b7f09c19d78c259d1f0baf843ec6989e
Percy Hanna <percyhanna@gmail.com>
2022-12-19 19:51:38 -0700

    Day 4, part 1

---
+++ day4/cleanup.rb
@@ -0,0 +1,21 @@
+lines = File.read("./input.txt").lines.map(&:chomp)
+
+covered_lines = lines.select do |line|
+  sets = line.split(",")
+
+  first, second = sets.map do |set|
+    a, b = set.split("-").map(&:to_i)
+
+    Range.new(a, b)
+  end
+
+  shared = first.to_a & second.to_a
+
+  if first.cover?(second) || second.cover?(first)
+    true
+  else
+    false
+  end
+end
+
+puts covered_lines.count

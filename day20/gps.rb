59a89b85f417f6ae80aa7789994c256c46dce10e
Percy Hanna <percyhanna@gmail.com>
2022-12-20 08:34:46 -0700

    Day 20 part 1

---
+++ day20/gps.rb
@@ -0,0 +1,24 @@
+lines = File.read("./test-input.txt").lines.map(&:chomp).map(&:to_i)
+lines = File.read("./input.txt").lines.map(&:chomp).map(&:to_i)
+
+ordered_lines = lines.each_with_index.map { |num, i| [num, i] }
+orig_lines = ordered_lines.dup
+
+orig_lines.each do |tuple|
+  number, order = tuple
+  next if number == 0
+
+  index = ordered_lines.index { |(num, ord)| ord == order }
+  ordered_lines.delete_at(index)
+
+  new_index = (index + number) % ordered_lines.count
+  ordered_lines.insert(new_index, tuple)
+end
+
+offset_0 = ordered_lines.index { |(num, ord)| num == 0 }
+code = [1_000, 2_000, 3_000].map do |offset|
+  ordered_lines[(offset + offset_0) % ordered_lines.count][0]
+end
+
+puts code.inspect
+puts code.sum.inspect
